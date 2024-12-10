2b1bea43070a32075ba4e25385f80d07104b41a9
cabmatthew <mcaballero3@luc.edu>
2024-12-10 00:52:37 -0600

    Got lomuto's parallel now

--- lomuto_parallel.cpp
+++ lomuto_parallel.cpp
@@ -7,34 +7,30 @@
 #include <omp.h>
 
 int partition(std::vector<int>& arr, int low, int high) {
-    int pivot = arr[low];
-    int i = low - 1, j = high + 1;
+    int pivot = arr[high];
+    int i = low - 1;
 
-    while (true) {
-        do {
+    for (int j = low; j < high; ++j) {
+        if (arr[j] < pivot) {
             i++;
-        } while (arr[i] < pivot);
-
-        do {
-            j--;
-        } while (arr[j] > pivot);
-
-        if (i >= j)
-            return j;
-
-        std::swap(arr[i], arr[j]);
+            std::swap(arr[i], arr[j]);
+        }
     }
+    std::swap(arr[i + 1], arr[high]);
+    return i + 1;
 }
 
-void quickSortHoareParallel(std::vector<int>& arr, int low, int high, int depth = 0) {
+void quickSortLomutoParallel(std::vector<int>& arr, int low, int high, int depth = 0) {
     if (low < high) {
-        int pi = partition(arr, low, high);
+        int pi = partition(arr, low, high);  
 
-        #pragma omp task shared(arr) if (depth < 4)
-        quickSortHoareParallel(arr, low, pi, depth + 1);
+        #pragma omp task shared(arr) if (depth < 4) 
+        quickSortLomutoParallel(arr, low, pi - 1, depth + 1);
 
-        #pragma omp task shared(arr) if (depth < 4)
-        quickSortHoareParallel(arr, pi + 1, high, depth + 1);
+        #pragma omp task shared(arr) if (depth < 4) 
+        quickSortLomutoParallel(arr, pi + 1, high, depth + 1);
+
+        #pragma omp taskwait 
     }
 }
 
@@ -52,14 +48,15 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    omp_set_num_threads(num_threads);
+
     std::vector<int> arr(array_size);
+
     std::srand(std::time(nullptr));
 
     for (int i = 0; i < array_size; ++i) {
-        arr[i] = std::rand() % 10000;
+        arr[i] = std::rand() % 10000; 
     }
-    
-    omp_set_num_threads(num_threads);
 
     std::cout << "Sorting an array of size: " << array_size << " with " << num_threads << " threads." << std::endl;
 
@@ -68,15 +65,13 @@ int main(int argc, char* argv[]) {
     #pragma omp parallel
     {
         #pragma omp single
-        {
-            quickSortHoareParallel(arr, 0, array_size - 1);
-        }
+        quickSortLomutoParallel(arr, 0, array_size - 1);
     }
 
     auto end_time = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration<double, std::milli>(end_time - start_time).count();
 
-    std::cout << "Parallelized Hoare QuickSort completed in " 
+    std::cout << "Parallel Lomuto's QuickSort completed in " 
               << std::fixed << std::setprecision(4) 
               << duration << " milliseconds." << std::endl;
 
